e81111499aad46faed86d644c6f1c1ca6fb711be
Cristina Tabares <cristina_tabares@hotmail.es>
2023-06-01 13:00:45 +0200

    navbar privado

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,26 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import { AuthContext } from "../context/auth.context"
+import { useContext } from "react"
+
 
 
 function Navbar() {
+  const navigate = useNavigate()
+  const {isLoggedIn, authenticateUser}= useContext(AuthContext)
+
+  const handleLogout = ()=>{
+    localStorage.removeItem("authToken")
+    authenticateUser()
+    navigate("/")
+  }
   return (
     <div>
-      <Link to={"/auth/signup"}>Registro</Link>
-      <Link to={"/auth/login"}>Login</Link>
+
+    {isLoggedIn && <Link to={"/profile"}>Perfil</Link> }
+    {isLoggedIn && <button onClick={handleLogout}>Cerrar sesion</button>}
+    {!isLoggedIn && <Link to={"/auth/signup"}>Registro</Link>}
+     {!isLoggedIn && <Link to={"/auth/login"}>Login</Link>}
+      
 
       
 
