950b8061417c56904c69bb900af75de511da013d
Cristina Tabares <cristina_tabares@hotmail.es>
2023-06-02 18:06:05 +0200

    pagina home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,34 @@
+import { useNavigate } from "react-router-dom"
+import axios from "axios"
+import { useState } from "react"
+import {RingLoader} from "react-spinners"
+import {getProductsService} from "../services/products.services"
 
 
 function Home() {
+  const navigate = useNavigate
+  const [allProducts, setAllProducts]= useState("")
+  const [isLoading, setIsLoading]= useState(true)
+  
+  const getData = async ()=>{
+    try {
+      const response = await getProductsService()
+      setAllProducts(response.data)
+      setIsLoading(false)
+          
+    } catch (error) {
+      navigate("/error")
+      
+    }
+  }
+  if(isLoading){
+    return(
+      <div className="spinner">
+          <RingLoader />
+        </div>
+    )
+  }
+
   return (
     <div>
       <h1>AQUI ESTA TU CASA</h1>
