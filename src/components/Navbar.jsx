8228cb85326928556bad2aeb585969c292441e36
Cristina Tabares <cristina_tabares@hotmail.es>
2023-06-02 17:48:27 +0200

    logo navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import { Link, useNavigate } from "react-router-dom"
 import { AuthContext } from "../context/auth.context"
 import { useContext } from "react"
 import Button from 'react-bootstrap/Button';
+import ironWineImg from "../assets/ironwine.png"
 
 
 
@@ -20,6 +21,7 @@ function Navbar({mostrarOcultarLogin}) { // pasamos funcion de mostrar/ocultar l
   return (
     <div>
 
+    <Link to={"/"}> <img src={ironWineImg} alt="logo" width={80} /> </Link>
     {isLoggedIn && <Link to={"/profile"}>Perfil</Link> }
     {isLoggedIn && <Button onClick={handleLogout}>Cerrar sesion</Button>}
     {!isLoggedIn && <Link to={"/auth/signup"}>Registro</Link>}
