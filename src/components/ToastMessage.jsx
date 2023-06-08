c89cc41e2ba61381a3d3493e74280503d59df853
Cristina Tabares <cristina_tabares@hotmail.es>
2023-06-08 10:10:19 +0200

    cambios toast

--- src/components/ToastMessage.jsx
+++ src/components/ToastMessage.jsx
@@ -13,8 +13,8 @@ function ToastMessage({messageTitle,message,setShow,show,bgColor,textColor}) {
       position={position}
       style={{ zIndex: 1 }}
     >
-      <Toast onClose={() => setShow(false)} show={show} delay={3000} autohide>
-          <Toast.Header style={{backgroundColor:bgColor,color:textColor}}>
+      <Toast onClose={() => setShow(false)} show={show} delay={2000} autohide>
+          <Toast.Header style={{backgroundColor:"#c8b589",color:textColor}}>
             <img
               src="holder.js/20x20?text=%20"
               className="rounded me-2"
